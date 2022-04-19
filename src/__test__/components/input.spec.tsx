9f3769a7629743b9da657cb520faef39509fdafd
Jonathan <jonathan.riosousa@gmail.com>
2022-04-19 07:58:47 -0300

    chore: adding tests

---
+++ src/__test__/components/input.spec.tsx
@@ -0,0 +1,61 @@
+import { fireEvent, render, waitFor } from '@testing-library/react';
+import React from 'react';
+
+import Input from '../../components/Input';
+
+jest.mock('@unform/core', () => {
+  return {
+    useField() {
+      return {
+        fieldName: 'email',
+        defaultValue: '',
+        error: '',
+        registerField: jest.fn(),
+      };
+    },
+  };
+});
+describe('Input component', () => {
+  it('should be able to render an input', () => {
+    const { getByPlaceholderText } = render(
+      <Input name="email" placeholder="E-mail" />,
+    );
+
+    expect(getByPlaceholderText('E-mail')).toBeTruthy();
+  });
+
+  it('should render highligh on input focus', async () => {
+    const { getByPlaceholderText, getByTestId } = render(
+      <Input name="email" placeholder="E-mail" />,
+    );
+
+    const inputElement = getByPlaceholderText('E-mail');
+    const containerElement = getByTestId('input-container');
+
+    fireEvent.focus(inputElement);
+
+    await waitFor(() => {
+      expect(containerElement).toHaveStyle('border-color: #ff9000;');
+      expect(containerElement).toHaveStyle('color: #ff9000;');
+    });
+  });
+
+  it('should keep input border hightlight when input filled', async () => {
+    const { getByPlaceholderText, getByTestId } = render(
+      <Input name="email" placeholder="E-mail" />,
+    );
+
+    const inputElement = getByPlaceholderText('E-mail');
+    const containerElement = getByTestId('input-container');
+
+    fireEvent.change(inputElement, {
+      target: { value: 'johndoe@example.com.br' },
+    });
+
+    fireEvent.blur(inputElement);
+
+    await waitFor(() => {
+      expect(containerElement).toHaveStyle('color: #ff9000;');
+    });
+  });
+});
